163600199a3eab84b67092617effee52d38da4d5
Leonardo Brito <leobri93@gmail.com>
2017-05-26 01:33:48 -0300

    [DataWorker] Dicionario de nota por estado pronto

---
+++ DataWorker/DataWorker/DataWorker/EnadeObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace DataWorker
+{
+    [DelimitedRecord(";")]
+    public class EnadeObject
+    {
+        public string codigoCurso;
+        public string tipoDeOrganizacao;
+        public string codigoMunicipio;
+        public string codigoUF;
+        public string codigoRegiao;
+        public string idadeAluno;
+        public string sexo;
+        public string anoFimSegundoGrau;
+        public string inicioGrad;
+        public string semestreDeGrad;
+        public string statusDoAluno; //Regular ou irregular
+        public string tipoDeInscricao; // Ingressante ou concluinte
+        public string tipoDePresenca;
+        public string notaConhecimentosGerais;
+        public string notaConhecimentosEspecificos;
+        [FieldConverter(ConverterKind.Double)]
+        public double? notaGeral;
+        public string faixaDeRenda;
+
+    }
+}
--- DataWorker/DataWorker/DataWorker/Program.cs
+++ DataWorker/DataWorker/DataWorker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileHelpers;
 
 namespace DataWorker
 {
@@ -10,6 +11,51 @@ namespace DataWorker
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> states = new Dictionary<string, string> {
+                { "11", "RO" },
+                { "12", "AC"},
+                { "13", "AM"},
+                { "14", "RR"},
+                { "15", "PA"},
+                { "16", "AP"},
+                { "17", "TO"},
+                { "21", "MA"},
+                { "22", "PI"},
+                { "23", "CE"},
+                { "24", "RN"},
+                { "25", "PB"},
+                { "26", "PE"},
+                { "27", "AL"},
+                { "28", "SE"},
+                { "29", "BA"},
+                { "31", "MG"},
+                { "32", "ES"},
+                { "33", "RJ"},
+                { "35", "SP"},
+                { "41", "PR"},
+                { "42", "SC"},
+                { "43", "RS"},
+                { "50", "MS"},
+                { "51", "MT"},
+                { "52", "GO"},
+                { "53", "DF"} };
+
+            Dictionary<string, List<double?>> stateGrades = new Dictionary<string, List<double?>>();
+             
+            FileHelperEngine<EnadeObject> engine = new FileHelperEngine<EnadeObject>();
+            var records = engine.ReadFile("C:\\Users\\leobr\\Dropbox\\UFF\\Visualização de dados\\microdados_enade_2014\\2.DADOS\\microdados_enade_2014_editado.csv");
+            foreach (EnadeObject currentRecord in records)
+            {
+                if (states.ContainsKey(currentRecord.codigoUF))
+                {
+                    if (stateGrades.ContainsKey(currentRecord.codigoUF))
+                        stateGrades[currentRecord.codigoUF].Add(currentRecord.notaGeral);
+                    else
+                    {
+                        stateGrades.Add(currentRecord.codigoUF, new List<double?> { currentRecord.notaGeral });
+                    }
+                }
+            }
         }
     }
 }
